aa571f191b6d841267a00a21c171293ab7c73fea
Natanael Feitosa <47866276+Natfeitosa@users.noreply.github.com>
2022-02-16 22:58:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Rest;
+using Discord.Net;
+using Discord.Commands;
+using Discord.Addons.Interactive;
+using NetEscapades.Configuration.Yaml;
+
+
+namespace Discord_bot
+{
+
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+             
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Discord_bot.Services;
+using NetEscapades.Configuration.Yaml;
+
+namespace Discord_bot
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(string [] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("_config.yml");
+            Configuration = builder.Build();
+        }
+        public static async Task RunAsync(string[] args)
+        {
+            var startup = new Startup(args);
+            await startup.RunAsync();
+        }
+        
+        public async Task RunAsync()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+            provider.GetRequiredService<CommandHandler>();
+
+            await provider.GetRequiredService<StartupService>().StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = Discord.LogSeverity.Verbose,
+                MessageCacheSize = 1000
+            }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    LogLevel = Discord.LogSeverity.Verbose,
+                    DefaultRunMode = RunMode.Async,
+                    CaseSensitiveCommands = false
+                }))
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<StartupService>()
+                .AddSingleton(Configuration);
+        }
+    }
+}
